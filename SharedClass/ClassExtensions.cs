5d9988409580170741010ffd832ec2a3f66379a5
Rio Cadeliña <cadelina13@gmail.com>
2022-04-26 20:26:29 +0800

    minor bugfixes

---
+++ SharedClass/ClassExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClass
+{
+    public static class ClassExtensions
+    {
+        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> enumerable, Func<T, TKey> keySelector)
+        {
+            return enumerable.GroupBy(keySelector).Select(grp => grp.First());
+        }
+    }
+}
